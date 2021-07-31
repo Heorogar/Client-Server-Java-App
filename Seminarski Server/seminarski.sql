f71d8295d3eba4dffc6ed7f4b932e9b6dfa5d516
alexander <alexandersoter@protonmail.com>
2021-07-31 15:54:50 +0200

    changed db

--- Seminarski Server/seminarski.sql
+++ Seminarski Server/seminarski.sql
@@ -16,6 +16,23 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`seminarski` /*!40100 DEFAULT CHARACTER 
 
 USE `seminarski`;
 
+/*Table structure for table `firme` */
+
+DROP TABLE IF EXISTS `firme`;
+
+CREATE TABLE `firme` (
+  `PK` int(10) NOT NULL AUTO_INCREMENT,
+  `Ime` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`PK`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `firme` */
+
+insert  into `firme`(`PK`,`Ime`) values 
+(1,'Microsoft'),
+(2,'Nordeus'),
+(3,'MSg');
+
 /*Table structure for table `korisnici` */
 
 DROP TABLE IF EXISTS `korisnici`;
@@ -43,16 +60,38 @@ CREATE TABLE `oglasi` (
   `Senioritet` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Datum_isteka` date DEFAULT NULL,
   `Opis` varchar(10000) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `Firma` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`PK`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `Firma` int(10) DEFAULT NULL,
+  PRIMARY KEY (`PK`),
+  KEY `Firma` (`Firma`),
+  CONSTRAINT `oglasi_ibfk_1` FOREIGN KEY (`Firma`) REFERENCES `firme` (`PK`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `oglasi` */
 
 insert  into `oglasi`(`PK`,`Pozicija`,`Senioritet`,`Datum_isteka`,`Opis`,`Firma`) values 
-(1,'Java developer','Senior','2021-12-21','Trazimo vrhunskog programera sa 10 godina iskustva. Nudimo dobre uslove i stoni tenis na poslu (hehe ionako necete imati vremena da ga igrate naivcine).','Msg'),
-(2,'Azure Cpp developer','Medior','2022-02-01','Mi smo Microsoft. Nemamo sta vise da kazemo, sve je jasno.','Microsoft'),
-(3,'C#','Junior','2022-04-14','Developeri su nam malo sranje ali imamo besplatnu hranu, super pocetne plate koje rastu godisnje za cak 5 evra ako se bas potrudite i manekenke koje vam seku jabuke, sta covek vise moze pozeleti u zivotu.','Nordeus');
+(1,'Java developer','Senior','2021-12-21','Trazimo vrhunskog programera sa 10 godina iskustva. Nudimo dobre uslove i stoni tenis na poslu (hehe ionako necete imati vremena da ga igrate naivcine).',3),
+(2,'Azure Cpp developer','Medior','2022-02-01','Mi smo Microsoft. Nemamo sta vise da kazemo, sve je jasno.',1),
+(3,'C#','Junior','2022-04-14','Developeri su nam malo sranje ali imamo besplatnu hranu, super pocetne plate koje rastu godisnje za cak 5 evra ako se bas potrudite i manekenke koje vam seku jabuke, sta covek vise moze pozeleti u zivotu.',2);
+
+/*Table structure for table `oglasi_po_firmama` */
+
+DROP TABLE IF EXISTS `oglasi_po_firmama`;
+
+CREATE TABLE `oglasi_po_firmama` (
+  `Oglas` int(10) NOT NULL,
+  `Firma` int(10) NOT NULL,
+  PRIMARY KEY (`Oglas`,`Firma`),
+  KEY `oglasi_po_firmama_ibfk_2` (`Firma`),
+  CONSTRAINT `oglasi_po_firmama_ibfk_1` FOREIGN KEY (`Oglas`) REFERENCES `oglasi` (`PK`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `oglasi_po_firmama_ibfk_2` FOREIGN KEY (`Firma`) REFERENCES `firme` (`PK`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `oglasi_po_firmama` */
+
+insert  into `oglasi_po_firmama`(`Oglas`,`Firma`) values 
+(1,3),
+(2,1),
+(3,2);
 
 /*Table structure for table `prijave` */
 
